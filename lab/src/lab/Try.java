488350e838f0e0a3159b30da24ceef357e5bc06f
Ayushi805 <59206413+Ayushi805@users.noreply.github.com>
2021-12-11 15:29:59 +0530

    Updated Try
    Updated from Github

--- lab/src/lab/Try.java
+++ lab/src/lab/Try.java
@@ -3,5 +3,6 @@ package lab;
 public class Try {
   public static void main(String[] args){
 	  System.out.print("Hi! I'm Ayushi");
+	  System.out.print("Hello! This line is being updated from github.");
   }
 }
